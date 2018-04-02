f3803914daedd15bbff702139edc7d2ed01e5aa3
fausto <fausto@faustos-MBP.lan>
2018-04-02 22:07:06 +0100

    Feed config

--- config/feed.php
+++ config/feed.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'feed_title' => "Stacktips",
-    'feed_description' => 'Your description',
-    'feed_logo' => 'http://example.com/images/brand/logo.png',
+    'feed_title' => "crypted-daily",
+    'feed_description' => 'We bring you all the latest streaming pricing data in the world of cryptocurrencies. Whether you are just interested in the Bitcoin price or you want to see the latest Ethereum volume, we have all the data available at your fingertips. Join our website, get daily market updates and gain access to the latest news and best reviews in the cryptocurrency arena.',
+    'feed_logo' => 'https://crypted-daily.com/asset/images/logo.jpg',
     'use_cache' => FALSE,
     'cache_key' => 'laravel-feed-cache-key',
     'cache_duration' => 3600,
